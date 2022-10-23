6c7cfcd9245cfe72e00e431c414fbc82313e7e45
Shalahuddin-Abdul <94779696+Shalahuddin-Abdul@users.noreply.github.com>
2022-10-23 09:20:33 +0700

    Update recFunct.py

--- Praktikum/UTS/recFunct.py
+++ Praktikum/UTS/recFunct.py
@@ -1,25 +1,33 @@
 import pandas as pd
 gpuData = pd.read_csv("gpuData.csv")
-# cpuData = pd.read_csv("Intel_CPUs.csv")
+
 
 gpuDedicated = gpuData.copy().loc[gpuData['Integrated']=='No']
 
 def filters(cat, x):
+    # copy parameter category yang di filter ke variable output
     output = gpuDedicated[(gpuDedicated[cat]>x)].copy().head(10)
     print(output[["Name", cat]])
 
 def gpuRec(x):
+    # VRAM > 1GB
     if x == 1:
-        filters("Memory(MB)", 1024) 
+        filters("Memory(MB)", 1024)
+    # VRAM > 4GB
     elif x == 2:
         filters("Memory(MB)", 4096)
+    # Pixel Rate > 120 GPixel/s
     elif x == 3:
-        filters("Pixel_Rate(GPixel/s)", 100)
+        filters("Pixel_Rate(GPixel/s)", 120)
+    # VRAM > 3GB
     elif x == 4:
         filters("Memory(MB)", 3072)
+    # VRAM > 2GB
     elif x == 5:
         filters("Memory(MB)", 2048)
+    # VRAM > 1.5GB
     elif x == 6:
-        filters("Memory(MB)", 1024)    
+        filters("Memory(MB)", 1536)    
+    # Pixel Rate > 100 GPixel/s
     elif x == 7:
         filters("Texture_Rate(GTexel/s)", 100)
