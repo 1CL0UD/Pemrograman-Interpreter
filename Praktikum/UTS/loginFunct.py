f4ead64cc3730e3f10bca2c1432be379c6d4413b
Hamzah Hanif <92318246+HamzahHanif@users.noreply.github.com>
2022-10-23 09:45:07 +0700

    Update loginFunct.py

--- Praktikum/UTS/loginFunct.py
+++ Praktikum/UTS/loginFunct.py
@@ -1,10 +1,17 @@
 def login():
     username = (input("Masukkan Username: ").upper())
+    i=0
     while True:
         try:
             x = int(input("Please enter a number: "))
+            i+=1
             if x == 6969:
                 print(username, " Berhasil Login")
                 break
+            else:
+                if i<=3:
+                    print("Pin salah, masukkan ulang pin yang benar." + str(i) + "/3")
+                elif i>3:
+                    exit("Terlalu banyak salah pin")
         except ValueError:
-            print("Oops! Pin yang dimasukkan bukan angka. Try again...")
+            print("Oops! Pin yang dimasukkan bukan angka.")
